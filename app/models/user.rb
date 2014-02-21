ccf4b3e04a7533ad3c8da24cea96ceddd3a9129a
Kerry Buckley <kerryjbuckley@gmail.com>
2014-02-21 12:13:50 +0000

    Make user attributes mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def self.create_or_update_with_omniauth_hash auth_hash
     info = auth_hash[:info]
     user = find_or_initialize_by provider: auth_hash[:provider], uid: auth_hash[:uid]
--- factories/user_factory.rb
+++ factories/user_factory.rb
@@ -1,3 +1,21 @@
 FactoryGirl.define do
-  factory :user
+  sequence :uid do |n|
+    800000000 + n
+  end
+  sequence :first_name do |n|
+    %w[Fred Alice Dave Lucy Bob Kate][n]
+  end
+
+  sequence :last_name do |n|
+    %w[Bloggs Smith Jones Davis Trubshaw][n]
+  end
+
+  factory :user do
+    provider "owa"
+    uid 
+    first_name
+    last_name
+    name { "#{first_name} #{last_name}" }
+    email { "#{first_name}.#{last_name}@example.com".downcase }
+  end
 end
