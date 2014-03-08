bb2b90cd2563a9a4904422519f928f2922aa03f4
Kerry Buckley <kerryjbuckley@gmail.com>
2014-03-08 17:04:52 +0000

    Use built-in Rails SSL forcing instead of middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-require "middleware/force_ssl"
-
 AdastralDevelopers::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -42,7 +40,7 @@ AdastralDevelopers::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -81,6 +79,4 @@ AdastralDevelopers::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.default_omniauth_strategy = "owa"
-
-  config.middleware.use :ForceSSL
 end
